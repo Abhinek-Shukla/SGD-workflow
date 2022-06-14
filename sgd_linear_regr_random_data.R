853dc218b33e6768302cbe81e411df6456b21f38
Abhinek <abhushukla@gmail.com>
2022-06-14 11:47:19 +0530

    Update sgd_linear_regr_random_data.R

--- sgd_linear_regr_random_data.R
+++ sgd_linear_regr_random_data.R
@@ -5,6 +5,7 @@ source("grad_lin.R")
 source("ebs_batch_mean.R")
 source("ibs_jasa_mean.R")
 source("sqrt_mat.R")
+
 Rep <- 1
 cutf <- 1000 #Dropping initial Iterates of SGD
 #Sample Size
@@ -31,12 +32,8 @@ sigm <- 1*diag(nparm)
 sqrt_sig <- sqrt_mat(sigm)
 
 forb_ebs <- forb_ibs <- numeric(Rep)
-ebs_mean <- ibs_mean <- vector("list", Rep)
-sqrt_ebs_mean <- sqrt_ibs_mean <- vector("list", Rep)
-inv_ebs_mean <- inv_ibs_mean <- vector("list", Rep)
-upp_ebs <- low_ebs <- upp_ibs <- low_ibs <- numeric(Rep)
-stand_err_ebs <- stand_err_ibs <- numeric(Rep)
-
+volm_ebs <- volm_ibs <- numeric(Rep)
+cover_ebs <- cover_ibs <- cover_orc <- numeric(Rep)
 #1000  Replications to obtain stable mses
 for(cn in 1 : Rep){
   
@@ -60,39 +57,23 @@ for(cn in 1 : Rep){
   asg <- colMeans(  sg_ct)
   n <- n - cutf
   #Variance-Covariance Estimate
-  ebs_mean[[cn]] <- ebs_batch_mean(sg_ct,alp)
-  ibs_mean[[cn]] <- ibs_jasa_mean(sg_ct,alp)
+  ebs_mean <- ebs_batch_mean(sg_ct,alp)
+  ibs_mean <- ibs_jasa_mean(sg_ct,alp)
   
   #Norm Difference 
-  forb_ebs[cn] <- sqrt(sum((ebs_mean[[cn]]-sigm)^2))
-  forb_ibs[cn] <- sqrt(sum((ibs_mean[[cn]]-sigm)^2))
+  forb_ebs[cn] <- sqrt(sum((ebs_mean-sigm)^2))/sqrt(sum(sigm^2))
+  forb_ibs[cn] <- sqrt(sum((ibs_mean-sigm)^2))/sqrt(sum(sigm^2))
   
-  # Square root of the matrix
-  sqrt_ebs_mean[[cn]] <- sqrt_mat(ebs_mean[[cn]])
-  sqrt_ibs_mean[[cn]] <- sqrt_mat(ibs_mean[[cn]])
+  # Volume of the matrix
+  volm_ebs[cn] <- (det(ebs_mean))^(1/nparm)
+  volm_ibs[cn] <- (det(ibs_mean))^(1/nparm)
   
-  # Inverse of the matrix
-  inv_ebs_mean[[cn]] <- solve(ebs_mean[[cn]])
-  inv_ibs_mean[[cn]] <- solve(ibs_mean[[cn]])
-  
-  #Standard Error 
-  ons <- (rep(1,nparm))
-  stand_err_ebs[cn] <- sqrt(t(ons)%*%ebs_mean[[cn]]%*%ons/n)
-  stand_err_ibs[cn] <- sqrt(t(ons)%*%ibs_mean[[cn]]%*%ons/n)
-  
-  #Upper and Lower Ends of the intervals
-  upp_ebs[cn] <- t(ons)%*%asg+qnorm(1-qlev/2)*stand_err_ebs[cn]
-  low_ebs[cn] <- t(ons)%*%asg-qnorm(1-qlev/2)*stand_err_ebs[cn]
-  upp_ibs[cn] <- t(ons)%*%asg+qnorm(1-qlev/2)*stand_err_ibs[cn]
-  low_ibs[cn] <- t(ons)%*%asg-qnorm(1-qlev/2)*stand_err_ibs[cn]
+#critical value calculation
+crt_val <- qchisq(0.95,df=nparm)
+
+cover_ebs[cn] <- as.numeric(n*t(asg-parm)%*%solve(ebs_mean)%*%(asg-parm)<=crt_val)
+cover_ibs[cn] <- as.numeric(n*t(asg-parm)%*%solve(ibs_mean)%*%(asg-parm)<=crt_val)
+cover_orc[cn] <- as.numeric(n*t(asg-parm)%*%solve(sigm)%*%(asg-parm)<=crt_val)  
+
 }
 
-#Smart batching EBS
-print(ebs_mean)
-
-#JASA Chen batching
-print(ibs_mean)
-
-
-print(forb_ebs)
-print(forb_ibs)
