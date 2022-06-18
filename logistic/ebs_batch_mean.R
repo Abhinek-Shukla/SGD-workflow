a594c2b17d7735b80319ef79c50cf7f4b2151f0e
Abhinek <abhushukla@gmail.com>
2022-06-18 13:21:44 +0530

    Update ebs_batch_mean.R

--- logistic/ebs_batch_mean.R
+++ logistic/ebs_batch_mean.R
@@ -13,7 +13,7 @@ an <- floor(n/bn)
 tot_mean <- colMeans(sg_ct)
 ebs <- matrix(rep(0,nparm^2),nrow=nparm,ncol=nparm)
 
-ebs <- mcse.multi(sg_ct,size=bn,r=2)$cov
+ebs <- mcse.multi(sg_ct,size=bn,r=1)$cov
 ebs <- ebs*(an-1)*bn/n
 
 add_trm <- numeric(nparm)
