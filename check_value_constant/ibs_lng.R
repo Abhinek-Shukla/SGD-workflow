193afd11f990c5429601b575e00a60cd6a97cf61
Abhinek <abhushukla@gmail.com>
2022-06-29 10:31:42 +0530

    Constant check completed.

--- check_value_constant/ibs_lng.R
+++ check_value_constant/ibs_lng.R
@@ -1,10 +1,11 @@
-ibs_lng <- function(dm,cns,alp){
+ibs_lng <- function(n,cns,alp){
   am <- numeric(1000)
   for(m in 1:10000){
     
     am[m] <- floor(cns*m^(2/(1-alp)))
     
   }
+  am <- am[am<n]
   am <- unique(am)
-  am[dm+1]
+  length(am)
 }
--- check_value_constant/varying_constant_study.R
+++ check_value_constant/varying_constant_study.R
@@ -14,44 +14,51 @@ for( k in 1:length(sq_n)){
   for (m in 1 : length(cns_sq)){
  
   leng_ebs[m,k] <- ebs_lng(n,cns=cns_sq[m],alp,bet_typ=1)
-  nparm         <- leng_ebs[m,k]
-  leng_ibs[m,k] <- ibs_lng(nparm,cns=cns_sq[m],alp)
+  
+  leng_ibs[m,k] <- ibs_lng(n,cns=cns_sq[m],alp)
 }
   
 }
 
-
-
-
 sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[1,],sampl_size_log10,type="l",ylim=c(3,12),xlab="Dimension",ylab="Sample size on log10 scale",main="c=0.01, beta type 1")
-lines(leng_ebs[1,],log10(leng_ibs[1,]),col="green")
+pdf("zero_pt_01_beta_1.pdf")
+plot(sampl_size_log10,leng_ebs[1,],type="l",ylim=c(20,2500),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[1,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+       col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
 
+pdf("zero_pt_1_beta_1.pdf")
+plot(sampl_size_log10,leng_ebs[2,],type="l",ylim=c(20,250),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[2,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[2,],sampl_size_log10,type="l",ylim=c(3,9),xlab="Dimension",ylab="Sample size on log10 scale",main="c=0.1, beta type 1")
-lines(leng_ebs[2,],log10(leng_ibs[2,]),col="green")
+pdf("zero_1_beta_1.pdf")
+plot(sampl_size_log10,leng_ebs[3,],type="l",ylim=c(2,30),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[3,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
 
-
-
-
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[3,],sampl_size_log10,type="l",ylim=c(2,6.5),xlab="Dimension",ylab="Sample size on log10 scale",main="c=1, beta type 1")
-lines(leng_ebs[3,],log10(leng_ibs[3,]),col="green")
-
-
-
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[4,],sampl_size_log10,type="l",ylim=c(1.5,6),xlab="Dimension",ylab="Sample size on log10 scale",main="c=2, beta type 1")
-lines(leng_ebs[4,],log10(leng_ibs[4,]),col="green")
+dev.off()
 
 
+pdf("zero_2_beta_1.pdf")
+plot(sampl_size_log10,leng_ebs[4,],type="l",ylim=c(2,25),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[4,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[5,],sampl_size_log10,type="l",ylim=c(.5,6),xlab="Dimension",ylab="Sample size on log10 scale",main="c=5, beta type 1")
-lines(leng_ebs[5,],log10(leng_ibs[5,]),col="green")
 
+pdf("zero_5_beta_1.pdf")
+plot(sampl_size_log10,leng_ebs[5,],type="l",ylim=c(1,20),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[5,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 ############################################################
 #New setup for beta
 sq_n <- seq(1e4,1e6,by=5e3)
@@ -64,8 +71,8 @@ for( k in 1:length(sq_n)){
   for (m in 1 : length(cns_sq)){
     
     leng_ebs[m,k] <- ebs_lng(n,cns=cns_sq[m],alp,bet_typ=2)
-    nparm         <- leng_ebs[m,k]
-    leng_ibs[m,k] <- ibs_lng(nparm,cns=cns_sq[m],alp)
+    
+    leng_ibs[m,k] <- ibs_lng(n,cns=cns_sq[m],alp)
   }
   
 }
@@ -74,34 +81,44 @@ for( k in 1:length(sq_n)){
 
 
 sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[1,],sampl_size_log10,type="l",ylim=c(3,14),xlab="Dimension",ylab="Sample size on log10 scale",main="c=0.01, beta type 2")
-lines(leng_ebs[1,],log10(leng_ibs[1,]),col="green")
+pdf("zero_pt_01_beta_2.pdf")
+plot(sampl_size_log10,leng_ebs[1,],type="l",ylim=c(20,8000),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[1,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
 
+pdf("zero_pt_1_beta_2.pdf")
+plot(sampl_size_log10,leng_ebs[2,],type="l",ylim=c(20,900),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[2,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[2,],sampl_size_log10,type="l",ylim=c(3,11),xlab="Dimension",ylab="Sample size on log10 scale",main="c=0.1, beta type 2")
-lines(leng_ebs[2,],log10(leng_ibs[2,]),col="green")
+pdf("zero_1_beta_2.pdf")
+plot(sampl_size_log10,leng_ebs[3,],type="l",ylim=c(5,80),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[3,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
 
-
-
-
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[3,],sampl_size_log10,type="l",ylim=c(3.5,8),xlab="Dimension",ylab="Sample size on log10 scale",main="c=1, beta type 2")
-lines(leng_ebs[3,],log10(leng_ibs[3,]),col="green")
-
-
-
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[4,],sampl_size_log10,type="l",ylim=c(3.5,7),xlab="Dimension",ylab="Sample size on log10 scale",main="c=2, beta type 2")
-lines(leng_ebs[4,],log10(leng_ibs[4,]),col="green")
+dev.off()
 
 
+pdf("zero_2_beta_2.pdf")
+plot(sampl_size_log10,leng_ebs[4,],type="l",ylim=c(2,40),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[4,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[5,],sampl_size_log10,type="l",ylim=c(1.5,6),xlab="Dimension",ylab="Sample size on log10 scale",main="c=5, beta type 2")
-lines(leng_ebs[5,],log10(leng_ibs[5,]),col="green")
 
+pdf("zero_5_beta_2.pdf")
+plot(sampl_size_log10,leng_ebs[5,],type="l",ylim=c(1,20),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[5,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 ########################################################
 
 sq_n <- seq(1e3,1e6,by=5e3)
@@ -114,8 +131,8 @@ for( k in 1:length(sq_n)){
   for (m in 1 : length(cns_sq)){
     
     leng_ebs[m,k] <- ebs_lng(n,cns=cns_sq[m],alp,bet_typ=3)
-    nparm         <- leng_ebs[m,k]
-    leng_ibs[m,k] <- ibs_lng(nparm,cns=cns_sq[m],alp)
+    
+    leng_ibs[m,k] <- ibs_lng(n,cns=cns_sq[m],alp)
   }
   
 }
@@ -124,34 +141,44 @@ for( k in 1:length(sq_n)){
 
 
 sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[1,],sampl_size_log10,type="l",ylim=c(3,14),xlab="Dimension",ylab="Sample size on log10 scale",main="c=0.01, beta type 3")
-lines(leng_ebs[1,],log10(leng_ibs[1,]),col="green")
+pdf("zero_pt_01_beta_3.pdf")
+plot(sampl_size_log10,leng_ebs[1,],type="l",ylim=c(2,9000),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[1,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
 
+pdf("zero_pt_1_beta_3.pdf")
+plot(sampl_size_log10,leng_ebs[2,],type="l",ylim=c(20,950),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[2,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[2,],sampl_size_log10,type="l",ylim=c(3,11),xlab="Dimension",ylab="Sample size on log10 scale",main="c=0.1, beta type 3")
-lines(leng_ebs[2,],log10(leng_ibs[2,]),col="green")
+pdf("zero_1_beta_3.pdf")
+plot(sampl_size_log10,leng_ebs[3,],type="l",ylim=c(2,100),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[3,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
 
-
-
-
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[3,],sampl_size_log10,type="l",ylim=c(3.,8.3),xlab="Dimension",ylab="Sample size on log10 scale",main="c=1, beta type 3")
-lines(leng_ebs[3,],log10(leng_ibs[3,]),col="green")
-
-
-
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[4,],sampl_size_log10,type="l",ylim=c(3.,7.5),xlab="Dimension",ylab="Sample size on log10 scale",main="c=2, beta type 3")
-lines(leng_ebs[4,],log10(leng_ibs[4,]),col="green")
+dev.off()
 
 
+pdf("zero_2_beta_3.pdf")
+plot(sampl_size_log10,leng_ebs[4,],type="l",ylim=c(2,50),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[4,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
-sampl_size_log10 <- log10(sq_n)
-plot(leng_ebs[5,],sampl_size_log10,type="l",ylim=c(2.5,6),xlab="Dimension",ylab="Sample size on log10 scale",main="c=5, beta type 3")
-lines(leng_ebs[5,],log10(leng_ibs[5,]),col="green")
 
+pdf("zero_5_beta_3.pdf")
+plot(sampl_size_log10,leng_ebs[5,],type="l",ylim=c(1,20),xlab="Sample Size ",ylab="Number of batches")
+lines(sampl_size_log10,leng_ibs[5,],col="green")
+legend( x="topleft",legend=c("EBS", "IBS"),
+        col=c("black", "green"), lty=c(1,1), cex=0.8)
+dev.off()
 
 
 
