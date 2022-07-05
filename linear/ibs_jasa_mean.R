d340e789c404e9545f87aa45dbd01755643a47e1
Dootika Vats <dootika1990@gmail.com>
2022-07-05 18:29:23 +0530

    cleaner bm estimates

--- linear/ibs_jasa_mean.R
+++ linear/ibs_jasa_mean.R
@@ -1,41 +1,18 @@
-ibs_jasa_mean <- function(sg_ct,alp) {
-n <- nrow(sg_ct)
-nparm <- ncol(sg_ct)
-# JASA Online BM Estimators
-for(m in 1:1000){
-  
-  am[m] <- floor(m^(2/(1-alp)))
-  
-}
-am <- c(am[am < n],n)
-ibs_jasa <- matrix(rep(0,nparm*(length(am)-1)),nrow=(length(am)-1),ncol=nparm)
-tot_mean <- rep(0,nparm)
+ibs_jasa_mean <- function(sg_ct, alp) 
+{
+  n <- nrow(sg_ct)
+  nparm <- ncol(sg_ct)
 
-#Equal batch size smart batching (EBS)
-for(k in 1 : (length(am)-1)){
-  strt_pt <- am[k]
-  end_pt <- am[k+1]-1
-  if(k==(length(am)-1)){ 
-    end_pt <- am[k+1]
-  }
-  
-  ibs_jasa[k,] <-  colSums(sg_ct[strt_pt : end_pt,])
-  tot_mean <- tot_mean + ibs_jasa[k,]
-}
-tot_mean <- tot_mean/(n)
-ibs_jasa_mean <- matrix(rep(0,nparm^2),nrow=nparm,ncol=nparm)
+  # JASA Online BM Estimators
+  am <- ceiling( .005*(1:1e3)^(2/(1-alp)))
+  am <- c(am[am < n], (n+1))
 
-for(k in 1 : (length(am)-1)){
-  
-  tmp_vl <- (ibs_jasa[k,]-(am[k+1]-am[k])*tot_mean)
- 
-   if(k == (length(am) - 1)){   
-  tmp_vl <- (ibs_jasa[k,]-(am[k+1]-am[k]+1)*tot_mean)
-    }
- 
-   ibs_jasa_mean <- ibs_jasa_mean + tmp_vl%*%t(tmp_vl)
-  
+  bm <- diff(am)
+  batch_means <- t(sapply(1:(length(am)-1), function(i) colMeans( matrix(sg_ct[am[i] : (am[i+1]-1), ], ncol = nparm ))))
+
+  batch_means <- bm*scale(batch_means, center = colMeans(sg_ct), scale = FALSE)
+  out <- (t(batch_means) %*% batch_means)/n
+
+  return(out)
 }
-ibs_jasa_mean <- ibs_jasa_mean/(n)
-return(ibs_jasa_mean)
-}
+
