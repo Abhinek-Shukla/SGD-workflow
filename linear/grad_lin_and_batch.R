8862d85fe43077df926a51c58d32d590103fe90e
Dootika Vats <dootika1990@gmail.com>
2022-07-09 15:35:56 +0530

    clean-up for linear

--- linear/grad_lin_and_batch.R
+++ linear/grad_lin_and_batch.R
@@ -5,14 +5,13 @@ grad_lin <- function(sg,y,x){
 ##########################################################################################################
 
 
-linear_batch_fn <- function(max_sam = 1e5, burn_in = 1000, nparm = 5, Rep = 1,  eta_cns = 0.5, qlev = 0.95, alp = .51, cns = c(0.1, 1) ){
-  sigm = diag(nparm)
-  sam_siz <- c(5e4,1e5,2e5,5e5,8e5,1e6,5e6,1e7)
+linear_batch_fn <- function(max_sam = 1e5, Rep = 1, nparm = 5, A = diag(nparm), cns = c(0.1, 1), eta_cns = 0.5, sam_siz = c(5e4,1e5,2e5,5e5,8e5,1e6,5e6,1e7), qlev = 0.95, alp = .51, burn_in = 1000){
+  sigm <- qr.solve(A) 
   sam_siz <- sam_siz[sam_siz <= max_sam]
   n <- sam_siz[length(sam_siz)] 
   
   
-  parm <- seq(1 / nparm, 1,length.out = nparm)
+  parm <- seq(1 / nparm, 1, length.out = nparm)
   crt_val <- qchisq(qlev, df = nparm)
   
   
@@ -22,16 +21,16 @@ linear_batch_fn <- function(max_sam = 1e5, burn_in = 1000, nparm = 5, Rep = 1,  
   
   # Sigma Matrix Stored with Square root
   
-  sqrt_sig <- sqrt_mat(sigm)
+  sqrt_sig <- sqrt_mat(A)
   
   cns_ln <- 3*length(cns)# 3 multiplied due to three types of beta under study
   forb_ibs <- volm_ibs <- cover_ibs <- forb_ibs_norm <- matrix(rep(0,length(sam_siz)*Rep), nrow = Rep, ncol = length(sam_siz))
   cover_orc<- matrix(rep(0,length(sam_siz)*Rep),nrow = Rep, ncol = length(sam_siz))
   
-  volm_ebs         <- forb_ebs <- forb_ebs_norm <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim = c(length(sam_siz), Rep, cns_ln))
-  cover_ebs        <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln))
-  volm_ebs_ls      <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln))
-  forb_ebs_norm_ls <- cover_ebs_ls  <- forb_ebs_ls <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln))
+  volm_ebs         <- forb_ebs <- forb_ebs_norm <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim = c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
+  cover_ebs        <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
+  volm_ebs_ls      <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
+  forb_ebs_norm_ls <- cover_ebs_ls  <- forb_ebs_ls <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
   
   
   #1000  Replications to obtain stable results
@@ -55,16 +54,18 @@ linear_batch_fn <- function(max_sam = 1e5, burn_in = 1000, nparm = 5, Rep = 1,  
       
     }
     
-    sg_ct <- sg[(burn_in + 1) : (n + burn_in),]
+    sg_ct_full <- sg[(burn_in + 1) : (n + burn_in),]
     
-    for ( smpl in 1 : length(sam_siz)) { 
-      asg <- colMeans(  sg_ct[1:sam_siz[smpl],])
+    for ( smpl in 1 : length(sam_siz)) 
+    { 
+      sg_ct <- sg_ct_full[1:sam_siz[smpl], ]
+      asg <- colMeans(sg_ct)
       
       #IBS and Oracle related coverages and volume
       
       ibs_mean     <- ibs_jasa_mean(sg_ct, alp)
-      forb_ibs[cn,smpl]  <- sqrt(sum((ibs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
-      forb_ibs_norm[cn,smpl] <- sqrt(sum((ibs_mean) ^ 2))
+      forb_ibs[cn,smpl]  <-  norm(ibs_mean - sigm, "F")/ norm(sigm, "F")  #sqrt(sum((ibs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
+      forb_ibs_norm[cn,smpl] <- norm(ibs_mean, "F")
       volm_ibs[cn,smpl]  <- (det(ibs_mean)) ^ (1 / nparm)
       cover_ibs[cn,smpl] <- as.numeric(n * t(asg - parm) %*% qr.solve(ibs_mean) %*% (asg - parm) <= crt_val)
       
@@ -75,14 +76,14 @@ linear_batch_fn <- function(max_sam = 1e5, burn_in = 1000, nparm = 5, Rep = 1,  
       #Different settings of EBS, for values of cns and three types of beta
       for( mk in 1 : length(cns)){ #Different values of constant cns
         for(bt_typ in 1 : 3){
-          ebs_mean                 <- ebs_batch_mean(sg_ct, alp, cns[mk], bt_typ, 1)
+          ebs_mean <- ebs_batch_mean(sg_ct, alp, cns[mk], bt_typ, 1)
           forb_ebs_norm[smpl,cn, count] <- sqrt(sum((ebs_mean) ^ 2))
           
           forb_ebs[smpl,cn, count]  <- sqrt(sum((ebs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
           volm_ebs[smpl,cn, count]  <- (det(ebs_mean) ) ^ (1 / nparm)
           cover_ebs[smpl,cn, count] <- as.numeric(n * t(asg - parm ) %*% qr.solve(ebs_mean ) %*% (asg - parm) <= crt_val)
           
-          ebs_mean                         <- ebs_batch_mean(sg_ct, alp, cns[mk], bt_typ, 2)
+          ebs_mean <- ebs_batch_mean(sg_ct, alp, cns[mk], bt_typ, 2)
           forb_ebs_norm_ls[smpl,cn, count] <- sqrt(sum((ebs_mean) ^ 2))
           
           forb_ebs_ls[smpl,cn, count]  <- sqrt(sum((ebs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
@@ -95,6 +96,6 @@ linear_batch_fn <- function(max_sam = 1e5, burn_in = 1000, nparm = 5, Rep = 1,  
       
     }
   }
-  fil_nam <- paste("linear/linear_indep_rep_",Rep,"_dim_",nparm,".RData",sep="")
+  fil_nam <- paste("out/linear_indep_n_",max_sam,"_dim_",nparm,".RData",sep="")
   save(forb_ibs_norm,forb_ebs_norm, forb_ebs_norm_ls,cover_orc,cover_ibs,cover_ebs,cover_ebs_ls,volm_ibs,volm_ebs,volm_ebs_ls,forb_ibs,forb_ebs,forb_ebs_ls,file=fil_nam)
 }
--- linear/sgd_linear_indep_run_file.R
+++ linear/sgd_linear_indep_run_file.R
@@ -1,12 +1,17 @@
 rm(list=ls())
+set.seed(1)
 library(MASS)
 
-source("linear/grad_lin_and_batch.R")
-source("ebs_batch_mean.R")
-source("ibs_jasa_mean.R")
-source("sqrt_mat.R")
+source("grad_lin_and_batch.R")
+source("./../ebs_batch_mean.R")
+source("./../ibs_jasa_mean.R")
+source("./../sqrt_mat.R")
 
-linear_batch_fn(max_sam = 1e5, burn_in = 1000, nparm = 5, Rep = 1,  eta_cns = 0.5, qlev = 0.95, alp = .51, cns = c(0.1, 1) )
-load("linear/linear_indep_rep_1_dim_5.RData")
+max_sam <- 1e6
+nparm <- 5
+linear_batch_fn(max_sam = max_sam, nparm = nparm, Rep = 2)
+
+foo <- paste("out/linear_indep_n_",max_sam,"_dim_",nparm,".RData",sep="")
+load(foo)
 
 
