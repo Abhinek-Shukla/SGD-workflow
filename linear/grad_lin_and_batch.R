2630d69aa35d3f912889643d23c376ac4fbb0ebb
Abhinek <abhushukla@gmail.com>
2022-07-10 21:33:23 +0530

    Linear Regression Code Final Independent and dim 5

--- linear/grad_lin_and_batch.R
+++ linear/grad_lin_and_batch.R
@@ -1,12 +1,16 @@
-grad_lin <- function(sg,y,x){
-  (x %*% (sg) - y) %*% x
-} 
-
-##########################################################################################################
-
-
-linear_batch_fn <- function(max_sam = 1e5, Rep = 1, nparm = 5, A = diag(nparm), cns = c(0.1, 1), eta_cns = 0.5, sam_siz = c(5e4,1e5,2e5,5e5,8e5,1e6,5e6,1e7), qlev = 0.95, alp = .51, burn_in = 1000){
-  sigm <- qr.solve(A) 
+linear_batch_fn <- function(max_sam = 1e5, Rep = 1, nparm = 5, A = diag(nparm), cns = c(0.1, 1), ncores_par = 3, eta_cns = 0.5, sam_siz = c(5e4,1e5,2e5,5e5,8e5,1e6,5e6,1e7), qlev = 0.95, alp = .51, burn_in = 1000){
+  grad_lin <- function(sg,y,x){
+    (x %*% (sg) - y) %*% x
+  } 
+  
+  ##########################################################################################################
+  comb <- function(x, ...) {
+    lapply(seq_along(x),
+           function(i) c(x[[i]], lapply(list(...), function(y) y[[i]])))
+  }
+  
+  
+   sigm <- qr.solve(A) 
   sam_siz <- sam_siz[sam_siz <= max_sam]
   n <- sam_siz[length(sam_siz)] 
   
@@ -24,17 +28,25 @@ linear_batch_fn <- function(max_sam = 1e5, Rep = 1, nparm = 5, A = diag(nparm), 
   sqrt_sig <- sqrt_mat(A)
   
   cns_ln <- 3*length(cns)# 3 multiplied due to three types of beta under study
-  forb_ibs <- volm_ibs <- cover_ibs <- forb_ibs_norm <- matrix(rep(0,length(sam_siz)*Rep), nrow = Rep, ncol = length(sam_siz))
-  cover_orc<- matrix(rep(0,length(sam_siz)*Rep),nrow = Rep, ncol = length(sam_siz))
+  forb_ibs <- volm_ibs <- cover_ibs <- forb_ibs_norm <- matrix(rep(0,length(sam_siz)*Rep), nrow = Rep, ncol = length(sam_siz), dimnames = list( 1 : Rep, sam_siz))
+  cover_orc<- matrix(rep(0,length(sam_siz)*Rep),nrow = Rep, ncol = length(sam_siz), dimnames = list( 1 : Rep, sam_siz))
   
   volm_ebs         <- forb_ebs <- forb_ebs_norm <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim = c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
   cover_ebs        <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
   volm_ebs_ls      <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
   forb_ebs_norm_ls <- cover_ebs_ls  <- forb_ebs_ls <- array(rep(0, cns_ln * Rep * length(sam_siz)), dim=c(length(sam_siz), Rep, cns_ln), dimnames = list(sam_siz, 1:Rep, 1:cns_ln))
   
+  library(doParallel) 
+  library(foreach)
   
-  #1000  Replications to obtain stable results
-  for(cn in 1 : Rep){
+  registerDoParallel(cores = min(Rep,ncores_par))  
+  
+  
+  
+  
+  #1000  Replications to obtain stable results under Parallel Setting
+    final_values <- foreach(cn = 1 : Rep, .combine = "comb", .packages = c("MASS","mcmcse"), .export = c( 'ebs_batch_mean', 'ibs_jasa_mean', 'sqrt_mat'), .multicombine=TRUE,
+                            .init=list(list(), list(),list(), list(),list(), list(),list(), list(),list(), list(),list(), list(),list() )) %dopar% {
     
     #Data Generated of Maximum Sample Size
     
@@ -77,25 +89,47 @@ linear_batch_fn <- function(max_sam = 1e5, Rep = 1, nparm = 5, A = diag(nparm), 
       for( mk in 1 : length(cns)){ #Different values of constant cns
         for(bt_typ in 1 : 3){
           ebs_mean <- ebs_batch_mean(sg_ct, alp, cns[mk], bt_typ, 1)
-          forb_ebs_norm[smpl,cn, count] <- sqrt(sum((ebs_mean) ^ 2))
+          forb_ebs_norm[smpl, cn, count] <- sqrt(sum((ebs_mean) ^ 2))
           
-          forb_ebs[smpl,cn, count]  <- sqrt(sum((ebs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
-          volm_ebs[smpl,cn, count]  <- (det(ebs_mean) ) ^ (1 / nparm)
-          cover_ebs[smpl,cn, count] <- as.numeric(n * t(asg - parm ) %*% qr.solve(ebs_mean ) %*% (asg - parm) <= crt_val)
+          forb_ebs[smpl, cn, count]  <- sqrt(sum((ebs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
+          volm_ebs[smpl, cn, count]  <- (det(ebs_mean) ) ^ (1 / nparm)
+          cover_ebs[smpl, cn, count] <- as.numeric(n * t(asg - parm ) %*% qr.solve(ebs_mean ) %*% (asg - parm) <= crt_val)
           
           ebs_mean <- ebs_batch_mean(sg_ct, alp, cns[mk], bt_typ, 2)
-          forb_ebs_norm_ls[smpl,cn, count] <- sqrt(sum((ebs_mean) ^ 2))
+          forb_ebs_norm_ls[smpl, cn, count] <- sqrt(sum((ebs_mean) ^ 2))
           
-          forb_ebs_ls[smpl,cn, count]  <- sqrt(sum((ebs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
-          volm_ebs_ls[smpl,cn, count]  <- (det(ebs_mean) ) ^ (1 / nparm)
-          cover_ebs_ls[smpl,cn, count] <- as.numeric(n * t(asg - parm ) %*% qr.solve(ebs_mean ) %*% (asg - parm) <= crt_val)
+          forb_ebs_ls[smpl, cn, count]  <- sqrt(sum((ebs_mean - sigm) ^ 2))/sqrt(sum(sigm ^ 2))
+          volm_ebs_ls[smpl, cn, count]  <- (det(ebs_mean) ) ^ (1 / nparm)
+          cover_ebs_ls[smpl, cn, count] <- as.numeric(n * t(asg - parm ) %*% qr.solve(ebs_mean ) %*% (asg - parm) <= crt_val)
           
           count = count + 1           
         }
       }
       
     }
+    list(forb_ibs[cn,],forb_ibs_norm[cn,], volm_ibs[cn,], cover_ibs[cn,], cover_orc[cn,],  forb_ebs_norm[, cn, ], forb_ebs[, cn, ], volm_ebs[, cn, ],  cover_ebs[, cn, ], forb_ebs_norm_ls[, cn, ], forb_ebs_ls[, cn, ], volm_ebs_ls[, cn, ],  cover_ebs_ls[, cn, ]  )
+    
   }
+  
+  for( k in 1 : Rep){
+    
+    forb_ibs[k,] <-  final_values[[1]][[k]]
+    forb_ibs_norm[k,] <- final_values[[2]][[k]]
+      volm_ibs[k,] <- final_values[[3]][[k]]
+      cover_ibs[k,] <- final_values[[4]][[k]]
+      cover_orc[k,] <- final_values[[5]][[k]]
+      forb_ebs_norm[, k, ] <- final_values[[6]][[k]]
+      forb_ebs[, k, ] <- final_values[[7]][[k]]
+      volm_ebs[, k, ] <- final_values[[8]][[k]]
+      cover_ebs[, k, ] <- final_values[[9]][[k]]
+      forb_ebs_norm_ls[, k, ] <- final_values[[10]][[k]]
+      forb_ebs_ls[, k, ] <- final_values[[11]][[k]]
+      volm_ebs_ls[, k, ] <-  final_values[[12]][[k]]
+      cover_ebs_ls[, k, ] <- final_values[[13]][[k]]
+  }
+  
+  
+  
   fil_nam <- paste("out/linear_indep_n_",max_sam,"_dim_",nparm,".RData",sep="")
   save(forb_ibs_norm,forb_ebs_norm, forb_ebs_norm_ls,cover_orc,cover_ibs,cover_ebs,cover_ebs_ls,volm_ibs,volm_ebs,volm_ebs_ls,forb_ibs,forb_ebs,forb_ebs_ls,file=fil_nam)
 }
--- linear/sgd_linear_indep_run_file.R
+++ linear/sgd_linear_indep_run_file.R
@@ -1,15 +1,17 @@
 rm(list=ls())
 set.seed(1)
 library(MASS)
-
+library(doParallel)
+library(foreach)
+library(mcmcse)
 source("grad_lin_and_batch.R")
 source("./../ebs_batch_mean.R")
 source("./../ibs_jasa_mean.R")
 source("./../sqrt_mat.R")
 
-max_sam <- 1e6
+max_sam <- 1e5
 nparm <- 5
-linear_batch_fn(max_sam = max_sam, nparm = nparm, Rep = 2)
+linear_batch_fn(max_sam = max_sam, nparm = nparm, Rep = 20, ncores_par = 30)#max(detectCores() - 5, 1)
 
 foo <- paste("out/linear_indep_n_",max_sam,"_dim_",nparm,".RData",sep="")
 load(foo)
