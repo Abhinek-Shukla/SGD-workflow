b91e7b24a21683795a41201ae842aa8e027de9e3
Abhinek <abhushukla@gmail.com>
2023-01-24 18:09:23 +0530

    Removed not required files.

--- check_value_constant/ibs_lng.R
+++
@@ -1,8 +0,0 @@
-ibs_lng <- function(n,cns,alp){
-  am <- numeric(1000)
-  # JASA Online BM Estimators
-  am <- ceiling( cns*(1:1e3)^(2/(1-alp)))
-  am <- c(am[am < n], (n+1))
-  am <- unique(am)
-  length(am)
-}
